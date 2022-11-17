6769aefb3ef724173fe258f429e04c1edc2d9930
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2022-11-17 10:12:55 -0800

    top level node rendering function and nodes types object

--- public/index.js
+++ public/index.js
@@ -1,1 +1,27 @@
-console.log('hello world')
+const NODES = Object.freeze({
+  HTML: 'HTML',
+  TEXT: '#text',
+  HEAD: 'HEAD',
+  COMMENT: '#comment',
+})
+
+function buildNode() {
+  // TODO 
+  // will need to either render a file or folder node
+  // going to have to create and stitch together a bunch of elements
+}
+
+function renderNode(nodeInfo) {
+  const { node, parent, depth = 0 } = nodeInfo
+  const children = [...node.childNodes]
+  const nodeName = node.nodeName
+
+  // don't render comments
+  if (nodeName === NODES.COMMENT) return
+  // account for HTML node (we don't want to render it, but need to render it's children)
+  const isHTMLNode = nodeName === NODES.HTML
+  const newParent = isHTMLNode ? parent : buildNode(nodeInfo)
+  const newDepth = isHTMLNode ? depth : depth + 1
+
+  children.forEach((child) => renderNode({ node: child, parent: newParent, depth: newDepth }))
+}
